4b358fb11bb7572c1e96c71700bf2ca25d086feb
checkMySwag <levchukcryptoeugene@gmail.com>
2019-02-19 16:22:15 +0300

    solution love-triangle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let pairs = 0;
+  let x = 0;
+  let y = 0;
+  let z = 0;
+  for (var i = 0; i < preferences.length; i++) {
+    x = i + 1;
+    y = preferences[i];
+    z = preferences[y - 1];
+    if (preferences[z - 1] === x) {
+      pairs++;
+    }
+  }
+  return Math.floor(pairs / 3);
 };
